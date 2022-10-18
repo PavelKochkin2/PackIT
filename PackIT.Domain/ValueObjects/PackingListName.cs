c897de809a70938fa51704774894433a29ae6d98
Pavel Kochkin <kach93@mail.ru>
2022-10-18 15:10:34 +0200

    added exception

--- PackIT.Domain/ValueObjects/PackingListName.cs
+++ PackIT.Domain/ValueObjects/PackingListName.cs
@@ -1,6 +1,18 @@
-﻿namespace PackIT.Domain.ValueObjects;
+﻿using PackIT.Domain.Exceptions;
 
-public class PackingListName
+namespace PackIT.Domain.ValueObjects;
+
+public record PackingListName
 {
+    public PackingListName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new EmptyPackingListNameException();
+        }
+
+        Value = value;
+    }
+
     public string Value { get; }
 }
