ccd20874f8bfb11ea630f59e5f98c175e2fe9952
Pavel Kochkin <kach93@mail.ru>
2022-10-20 19:26:40 +0200

    added PackingListFactory

--- PackIT.Domain/Factories/PackingListFactory.cs
+++ PackIT.Domain/Factories/PackingListFactory.cs
@@ -1,21 +1,32 @@
 ﻿using PackIT.Domain.Consts;
 using PackIT.Domain.Entities;
+using PackIT.Domain.Policies;
 using PackIT.Domain.ValueObjects;
 
 namespace PackIT.Domain.Factories;
 
 public class PackingListFactory : IPackingListFactory
 {
+    private readonly IEnumerable<IPackingItemsPolicy> _policies;
+
+    public PackingListFactory(IEnumerable<IPackingItemsPolicy> policies)
+    {
+        _policies = policies;
+    }
+
     public PackingList Create(PackingListId id, PackingListName name, 
         Localization localization)
     {
-        throw new NotImplementedException();
+        return new PackingList(id, name, localization);
     }
 
     public PackingList CreateWithDefaultItems(PackingListId id, 
         PackingListName name, TravelDays days, Gender gender,
         Temperature temperature, Localization localization)
     {
-        throw new NotImplementedException();
+        var data = new PolicyData(days, gender, temperature, localization);
+
+        var applicablePolicies =
+            _policies.Where(p => p.IsApplicable(data));
     }
 }
---
+++ PackIT.Domain/Policies/Universal/BasicPolicy.cs
@@ -0,0 +1,28 @@
+﻿using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Policies.Universal;
+
+internal sealed class BasicPolicy : IPackingItemsPolicy
+{
+    private const uint MaxQuantityOfClothes = 7;
+
+    public bool IsApplicable(PolicyData data) => true;
+
+    public IEnumerable<PackingItem> GenerateItems(PolicyData data)
+    {
+        return new List<PackingItem>()
+        {
+            new PackingItem("Pants", Math.Min(data.Days, MaxQuantityOfClothes)),
+            new PackingItem("Socks", Math.Min(data.Days, MaxQuantityOfClothes)),
+            new PackingItem("T-Shirt", Math.Min(data.Days, MaxQuantityOfClothes)),
+            new PackingItem("Trousers", data.Days < 7 ? 1u : 2u),
+            new PackingItem("Shampoo", 1),
+            new PackingItem("Toothbrush", 1),
+            new PackingItem("Toothpaste", 1),
+            new PackingItem("Towel", 1),
+            new PackingItem("Pag pack", 1),
+            new PackingItem("Passport", 1),
+            new PackingItem("Phone Charger", 1),
+        };
+    }
+}
