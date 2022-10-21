de18caf98c20dbb4f58e2c263d9ed92cc6888a8b
Pavel Kochkin <kach93@mail.ru>
2022-10-21 13:24:13 +0200

    added scutor package

--- PackIT.Shared/Extensions.cs
+++ PackIT.Shared/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PackIT.Shared.Abstractions.Commands;
 using PackIT.Shared.Commands;
+using System.Reflection;
 
 namespace PackIT.Shared;
 
@@ -8,7 +9,15 @@ public static class Extensions
 {
     public static IServiceCollection AddCommands(this IServiceCollection services)
     {
+        var assembly = Assembly.GetCallingAssembly();
+
         services.AddSingleton<ICommandDispatcher, InMemoryCommandDispatcher>();
+
+        services.Scan(s => s.FromAssemblies(assembly)
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
 
         return services;
     }
