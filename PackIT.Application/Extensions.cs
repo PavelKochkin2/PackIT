53ae104965b50624da080ad0ceb9307edb927636
Pavel Kochkin <kach93@mail.ru>
2022-10-23 10:01:32 +0200

    added ext

---
+++ PackIT.Application/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+using Microsoft.Extensions.DependencyInjection;
+using PackIT.;
+
+namespace PackIT.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddCommands();
+        return services;
+    }
+}
