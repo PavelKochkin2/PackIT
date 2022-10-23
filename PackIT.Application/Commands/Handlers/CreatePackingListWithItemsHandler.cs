b93909b73f77b1012bb396f30b8cf205ad7797cb
Pavel Kochkin <kach93@mail.ru>
2022-10-23 10:51:21 +0200

    worked on CreatePackingListWithItemsHandler

--- PackIT.Application/Commands/Handlers/CreatePackingListWithItemsHandler.cs
+++ PackIT.Application/Commands/Handlers/CreatePackingListWithItemsHandler.cs
@@ -1,10 +1,24 @@
-﻿using PackIT.Shared.Abstractions.Commands;
+﻿using PackIT.Application.Services;
+using PackIT.Domain.Factories;
+using PackIT.Domain.Repositories;
+using PackIT.Shared.Abstractions.Commands;
 
 namespace PackIT.Application.Commands.Handlers;
 
 public class CreatePackingListWithItemsHandler :
     ICommandHandler<CreatePackingListWithItems>
 {
+    private readonly IPackingListRepository _repository;
+    private readonly IPackingListFactory _factory;
+    private readonly IPackingListReadService _readService;
+
+    public CreatePackingListWithItemsHandler(IPackingListRepository repository, IPackingListFactory factory, IPackingListReadService readService)
+    {
+        _repository = repository;
+        _factory = factory;
+        _readService = readService;
+    }
+
     public Task HandleAsync(CreatePackingListWithItems command)
     {
         //check if the list exists in db(we don't allow name duplicates)
