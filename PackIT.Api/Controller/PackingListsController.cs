d6a987c306e49f7984b4d36444f732099064f22f
Pavel Kochkin <pavel.kochkin.ext@publicismedia.com>
2022-11-08 18:42:16 +0100

    added contolllers

---
+++ PackIT.Api/Controller/PackingListsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PackIT.Application.DTO;
+using PackIT.Application.Queries;
+using PackIT.Shared.Abstractions.Commands;
+using PackIT.Shared.Abstractions.Queries;
+
+namespace PackIT.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PackingListsController : ControllerBase
+{
+    private readonly ICommandDispatcher _commandDispatcher;
+    private readonly IQueryDispatcher _queryDispatcher;
+
+    public PackingListsController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+        _queryDispatcher = queryDispatcher;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<PackingListDto>> Get(
+        [FromRoute] GetPackingList query)
+    {
+        var result = await _queryDispatcher.QueryAsync(query);
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<IEnumerable<PackingListDto>>> Get(
+        [FromQuery] SearchPackingLists query)
+    {
+        var result = await _queryDispatcher.QueryAsync(query);
+
+        if (result is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+}
