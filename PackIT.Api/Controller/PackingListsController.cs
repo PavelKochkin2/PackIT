3573b3c1a2038f2f3bc7eece78f93fc38e3214be
Pavel Kochkin <pavel.kochkin.ext@publicismedia.com>
2022-11-10 18:36:07 +0100

    controller changes

--- PackIT.Api/Controller/PackingListsController.cs
+++ PackIT.Api/Controller/PackingListsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PackIT.Application.Commands;
 using PackIT.Application.DTO;
 using PackIT.Application.Queries;
 using PackIT.Shared.Abstractions.Commands;
@@ -34,5 +35,39 @@ public class PackingListsController : BaseController
         var result = await _queryDispatcher.QueryAsync(query);
 
         return OkOrNotFound(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody]
+        CreatePackingListWithItems command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return CreatedAtAction
+            (nameof(Get), new { id = command.id }, null);
+    }
+
+    [HttpPost("{packingListId:guid}/items/{name}/pack")]
+    public async Task<IActionResult> Post([FromBody]
+        PackItem command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
+    [HttpDelete("{packingListId:guid}/items/{name}")]
+    public async Task<IActionResult> Put([FromBody]
+        RemovePackingItem command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Put([FromBody]
+        DeletePackingList command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
     }
 }
